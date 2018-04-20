4c4eb0bef9b8f728f60a04395be11e6caf41169c
FluffyMcFlop <38464092+FluffyMcFlop@users.noreply.github.com>
2018-04-20 15:01:32 +0200

    Add files via upload

---
+++ results.php
@@ -0,0 +1,49 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<link rel="stylesheet" type="text/css" href="indexstyle.css">
+	<link rel="stylesheet" type="text/css" href="resultstyle.css">
+    <meta charset="utf-8" />
+    <title></title>
+</head>
+<body>
+<header>
+		<image id="norgesbakgrunn" src="Bilder/Norgesbakgrunn.jpg" alt="Patriotisk bilde">
+	
+		<ul class="navbar">
+			<li class="navoptions"><a class="navlink" href="mainpage.html">Hjem</a></li>
+			<li class="navoptions"><a class="navlink" href="work.html">Arbiedsmarked</a></li>
+			<li class="navoptions"><a class="navlink" href="about.html">Om oss</a></li>
+			<li class="navoptions"><a class="navlink" href="login.html">Logg inn</a></li>
+		</ul>
+</header>
+
+<div class="Content">
+		<h1 align="Center">Arbeidsforklaring</h1>
+    
+<fieldset>
+    <legend>Vedhogging</legend>
+    <br>
+    <p class="workdesc">Arbeidsoppgaven går ut på: Gammel dame på 80, trenger hjelp til å hogge ved. <br>
+    Har øks i vedskjulet, men den er ganske gammel.</p>
+    
+
+    <div class="profileid">
+        <p>Bente Halvorsen</p>
+        <p>Utlyst 2018.04.17</p>¨
+
+        <p>Tlf:94457345</p>
+        <p>E-mail: bentehalvorsen@gmail.com</p>
+    </div>
+</fieldset>
+		
+
+	
+</div>
+
+
+
+
+</body>
+</html>
